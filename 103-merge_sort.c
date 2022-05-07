8445af7b846a059d2b027f29f38b3c0d0c084e5a
AdamNB-sys <a1adnat@gmail.com>
2022-05-07 16:41:26 -0500

    merge sort project files + adding prototype to .h file

---
+++ 103-merge_sort.c
@@ -0,0 +1,13 @@
+#include "sort.h"
+
+
+/**
+ * Merge sort - sorts an array of integers in ascending order
+ * using the top-down merge sort algorithm
+ * @array: array to be sorted
+ * @size: size of the array
+ *
+ * Return: void
+ */
+
+void merge_sort(int *array, size_t size)
